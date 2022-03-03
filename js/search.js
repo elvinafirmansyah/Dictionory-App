edc6751f9a009096927a9d9a57e805b64720aa69
elvinafirmansyah <elvinafirmansyah@gmail.com>
2022-03-03 13:34:15 +0700

    update search audio

--- js/search.js
+++ js/search.js
@@ -6,8 +6,9 @@ const kirimBtn = document.getElementById('btnsearch');
 const atasSection = document.getElementById('subtitle');
 let audio;
 
-getData('time');
+// first data
 
+getData('fact');
 
 async function getData(words) {
     const resp = await fetch(APIURL + words);
@@ -129,7 +130,7 @@ function secondPlayAudio() {
 // third data
 const thirdMain = document.getElementById('main3');
 
-getThirdData('fact');
+getThirdData('time');
 
 async function getThirdData(threewords) {
   const resp = await fetch(APIURL + threewords);
@@ -178,13 +179,74 @@ function createThirdData(thridword) {
   </div>
   `;
 
-  thridAudio = new Audio(thridword[0].phonetics[0].audio);
+  thridAudio = new Audio(thridword[0].phonetics[3].audio);
 
   thirdMain.innerHTML = thridCardHTML;
 }
 
 function thridPlayAudio() {
   thridAudio.play();
+}
+
+// fourth data
+const fourthMain = document.getElementById('main4');
+
+getFourthData('aircraft');
+
+async function getFourthData(fourthwords) {
+  const resp = await fetch(APIURL + fourthwords);
+  const respData = await resp.json();
+
+  createFourthData(respData);
+}
+
+function createFourthData(fourthword) {
+  if (!fourthword.ok) {
+   const thridError = `
+      <div class="max-w-md mx-auto overflow-hidden md:max-w-2xl">
+        <div class="detail mt-1 flex p-1">
+          <h2 class="text-red-500 text-normal">Can't find the meaning. Please, try to search for another word.</h2>
+        </div>
+      </div>
+    `;
+    fourthMain.innerHTML = thridError;
+  }
+  const fourthCardHTML = `
+    <div class="max-w-md mx-auto bg-gray-100 rounded-xl overflow-hidden md:max-w-2xl">
+    <div class="p-5">
+
+      <div class="flex justify-between items-stretch">
+        <div class="word">
+          <h2 class="font-bold text-2xl" id="word">${fourthword[0].word}</h2>
+          <div class="detail mt-1 flex">
+            <p id="partOfSpeech" class="text-sky-400/75 text-md pr-2">${fourthword[0].meanings[0].partOfSpeech}</p>
+            <p id="phonetics" class="text-gray-400 text-md">${fourthword[0].phonetics[0].text}</p>
+          </div>
+        </div>
+        <button class="p-3 text-black hover:text-sky-400" onclick="fourthPlayAudio();"><i class="bi bi-volume-up-fill"></i></button>
+      </div>
+
+      <div class="bg-sky-300/50 rounded-lg pt-3 pb-1 px-3 my-3">
+        <p class="font-semibold text-lg">Definition:</p>
+        <p class="word text-sm mb-3" id="definition">${fourthword[0].meanings[0].definitions[0].definition}</p>
+      </div>
+
+      <div class="bg-sky-300/50 rounded-lg pt-3 pb-1 px-3 my-3">
+        <p class="font-semibold text-lg">Example:</p>
+        <p class="word text-sm mb-3" id="example">${fourthword[0].meanings[0].definitions[0].example}</p>
+      </div>
+
+    </div>
+  </div>
+  `;
+
+  fourthAudio = new Audio(fourthword[0].phonetics[1].audio);
+
+  fourthMain.innerHTML = fourthCardHTML;
+}
+
+function fourthPlayAudio() {
+  fourthAudio.play();
 }
 
 form.addEventListener("submit", (e) => {
@@ -197,6 +259,7 @@ form.addEventListener("submit", (e) => {
     }
     secondMain.classList.add('hidden');
     thirdMain.classList.add('hidden');
+    fourthMain.classList.add('hidden');
 });
 
 kirimBtn.addEventListener ('click', () => {
